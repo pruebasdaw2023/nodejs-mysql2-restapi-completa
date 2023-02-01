31415982a571f3a7119fb7fb2a728d188a61b34b
Luis García <pruebasdaw2021@gmail.com>
2023-02-01 14:09:19 +0100

    Creada base de datos companydb

---
+++ db/database.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS companydb;
+CREATE DATABASE companydb CHARSET utf8mb4;
+USE companydb;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) DEFAULT NULL,
+    salary INT DEFAULT NULL,
+    PRIMARY KEY(id)
+);

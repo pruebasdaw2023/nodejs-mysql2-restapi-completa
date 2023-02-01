4d3755d6961185e663149e2fa1941c888da288bf
Luis García <pruebasdaw2021@gmail.com>
2023-02-01 13:58:21 +0100

    Endpoints funcionales al inicio

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.send('Hola mundo');
+});
+
+router.get('/employees', (req, res, next) => {
+  res.send('Obteniendo empleados');
+});
+router.post('/employees', (req, res, next) => {
+  res.send('Creando empleados');
+});
+router.put('/employees', (req, res, next) => {
+  res.send('Actualizando empleados');
+});
+router.delete('/employees', (req, res, next) => {
+  res.send('Eliminando empleados');
 });
 
 module.exports = router;

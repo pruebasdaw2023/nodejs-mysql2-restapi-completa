02af2be8a9f84ba0d6c5b2c47fba74722730863d
Luis García <pruebasdaw2021@gmail.com>
2023-02-03 10:17:29 +0100

    Implementados getemployees y createemployees

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -1,9 +1,20 @@
-const getEmployees = (req, res, next) => {
-    res.send('Obteniendo empleados');
+const pool = require("../db.js");
+
+const getEmployees = async (req, res, next) => {
+    const [row] = await pool.query('SELECT * FROM employee')
+    res.json(row);
 }
 
-const createEmployees = (req, res, next) => {
-    res.send('Creando empleados');
+const createEmployees = async (req, res, next) => {
+
+  const { name, salary } = req.body
+    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
+    //console.log(name, salary)
+    res.send({ 
+      id: rows.insertId,
+      name,
+      salary
+     });
 }
 
 const updateEmployees = (req, res, next) => {
